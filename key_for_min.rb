26044416e22279e90b1cb61f0f2f8319bf6cec34
beaming-subroutine-5164 <ben.garlock@gmail.com>
2020-06-01 15:40:15 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
-end
+  if name_hash == {}
+    return NIL
+  else
+    values_array = name_hash.collect{|key, value| value}.sort
+    name_hash.each{|key, value| return key if value == values_array[0]}
+  end
+end 
